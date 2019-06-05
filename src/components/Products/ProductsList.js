92cb0d73b0e13da0e50b0fc24408ef3dbaba1d2a
silvanaweb <silvanasono@yahoo.it>
2019-06-05 20:16:19 +0100

    secure handlebars

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -6,58 +6,52 @@ import selectProducts from "../../store/selectors/products";
 import { ProductListItem } from "./ProductListItem";
 import "./style.css";
 
-const ProductsList = ({
-  products,
-  setCategory,
-  setTextFilter,
-  textFilter,
-  match: { params }
-}) => {
-  useEffect(() => {
-    const { category } = params;
-    setCategory(category);
-  }, [params]);
+const ProductsList = ({ products, setCategory, setTextFilter, textFilter, match: { params } }) => {
+	useEffect(() => {
+		const { category } = params;
+		setCategory(category);
+	}, [params]);
 
-  const onChange = e => {
-    const text = e.target.value;
-    setTextFilter(text);
-  };
-  return (
-    <>
-      <div className="ProductsList__filter">
-        <DebounceInput
-          value={textFilter}
-          className="Input"
-          placeholder="Filter..."
-          minLength={1}
-          debounceTimeout={400}
-          onChange={onChange}
-        />
-      </div>
-      <div>
-        {products.map(({ new: newItem, title }, idx) => (
-          <ProductListItem key={idx} newItem={newItem} title={title} />
-        ))}
-      </div>
-    </>
-  );
+	const onChange = e => {
+		const text = e.target.value;
+		setTextFilter(text);
+	};
+	return (
+		<div>
+			<div className="ProductsList__filter">
+				<DebounceInput
+					value={textFilter}
+					className="Input"
+					placeholder="Filter..."
+					minLength={1}
+					debounceTimeout={400}
+					onChange={onChange}
+				/>
+			</div>
+			<div>
+				{products.map(({ new: newItem, title }, idx) => (
+					<ProductListItem key={idx} newItem={newItem} title={title} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 export { ProductsList };
 
 const mapStateToProps = (state, props) => {
-  return {
-    products: selectProducts(state.products, state.filters),
-    textFilter: state.filters.text
-  };
+	return {
+		products: selectProducts(state.products, state.filters),
+		textFilter: state.filters.text,
+	};
 };
 
 const mapDispatchToProps = dispatch => ({
-  setCategory: category => dispatch(setCategory(category)),
-  setTextFilter: text => dispatch(setTextFilter(text))
+	setCategory: category => dispatch(setCategory(category)),
+	setTextFilter: text => dispatch(setTextFilter(text)),
 });
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(ProductsList);
